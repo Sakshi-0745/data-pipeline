43a90da745dd6bf18df93702b85cfaa16824bf45
Sakshi-0745 <kalashettisakshi@gmail.com>
2025-12-31 10:59:30 +0530

    Reference existing S3 buckets using Terraform data sources

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "raw_bucket" {
+data "aws_s3_bucket" "raw_bucket" {
   bucket = "event-raw-data-pipeline"
 }
 
-resource "aws_s3_bucket" "processed_bucket" {
+data "aws_s3_bucket" "processed_bucket" {
   bucket = "processed-event-data"
 }
